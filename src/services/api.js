08479df675f75e2f8fe5bfe32cec1e350f9e5927
Alberto Diaz <aado29@gmail.com>
2019-07-14 17:47:16 -0400

    feat(api service): add general methods

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,46 @@
 import axios from 'axios';
+
+const request = axios.create({
+  baseURL: `${process.env.REACT_APP_API_BASE_URL}/api/v1`,
+});
+
+request.defaults.headers.post['Accept'] = 'application/json';
+request.defaults.headers.post['Content-Type'] = 'application/json';
+
+export const getCounters = () => {
+  return request
+    .get('/counters')
+    .then(data => data.data);
+}
+
+export const addCounter = (params) => {
+  return request
+    .post('/counter', JSON.stringify(params))
+    .then(data => data.data);
+}
+
+export const increaseCounter = (idCounter) => {
+  return request
+    .post('/counter/inc', JSON.stringify({id: idCounter}))
+    .then(data => data.data);
+}
+
+export const decreaseCounter = (idCounter) => {
+  return request
+    .post('/counter/dec', JSON.stringify({id: idCounter}))
+    .then(data => data.data);
+}
+
+export const removeCounter = (idCounter) => {
+  return request
+    .delete('/counter/dec', JSON.stringify({id: idCounter}))
+    .then(data => data.data);
+}
+
+export default {
+  getCounters,
+  addCounter,
+  increaseCounter,
+  decreaseCounter,
+  removeCounter,
+};
