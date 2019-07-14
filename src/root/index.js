7c3407bbf9c81a022be6763a36a3fdd35c74a002
Alberto Diaz <aado29@MacBook-Pro-de-Alberto.local>
2019-07-14 15:38:36 -0400

    chrore(general): initial commit

---
+++ src/root/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Root.scss';
+
+function Root() {
+  return (
+    <div className="root">
+      <h1>Initial View</h1>
+    </div>
+  );
+}
+
+export default Root;
