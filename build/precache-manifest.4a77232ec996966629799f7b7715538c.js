52eafc093785fc5cbb50250575efa42042566656
Alberto Diaz <aado29@gmail.com>
2019-07-15 10:53:00 -0400

    build: add builed files

---
+++ build/precache-manifest.4a77232ec996966629799f7b7715538c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf893b9ffca949e466eee459372e9118",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d72dcf0c74602ab9991",
+    "url": "/static/css/main.5c25fa6b.chunk.css"
+  },
+  {
+    "revision": "8e4c60b03e595e489000",
+    "url": "/static/js/2.58c1f447.chunk.js"
+  },
+  {
+    "revision": "9d72dcf0c74602ab9991",
+    "url": "/static/js/main.28d436ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
